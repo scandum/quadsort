71a3ab668aaa6627f73c889b754501aa0f1f79a0
scandum <ivdhoven@gmail.com>
2022-06-08 21:31:12 +0200

    1.1.5.3

--- src/quadsort.c
+++ src/quadsort.c
@@ -421,71 +421,78 @@ size_t FUNC(quad_swap)(VAR *array, size_t nmemb, CMPFUNC *cmp)
 
 // quad merge support routines
 
-void FUNC(forward_merge)(VAR *dest, VAR *from, size_t block, CMPFUNC *cmp)
+void FUNC(galloping_merge)(VAR *dest, VAR *from, size_t block, CMPFUNC *cmp)
 {
-	VAR *ptl, *ptr, *m, *e; // left, right, middle, end
-	size_t x, y;
+	VAR *ptl, *tpl, *ptr, *tpr, *ptd, *tpd;
+	size_t loop, x, y;
 
 	ptl = from;
 	ptr = from + block;
-	m = ptr - 1;
-	e = ptr + block - 1;
+	tpl = ptr - 1;
+	tpr = ptr + block - 1;
 
-	if (cmp(m, e - block / 4) <= 0)
+	if (cmp(ptl + 15, ptr) > 0 && cmp(ptl, ptr + 15) <= 0 && cmp(tpl, tpr - 15) > 0 && cmp(tpl - 15, tpr) <= 0)
 	{
-		while (ptl < m - 1)
-		{
-			if (cmp(ptl + 1, ptr) <= 0)
-			{
-				*dest++ = *ptl++; *dest++ = *ptl++;
-			}
-			else if (cmp(ptl, ptr + 1) > 0)
-			{
-				*dest++ = *ptr++; *dest++ = *ptr++;
-			}
-			else 
-			{
-				x = cmp(ptl, ptr) <= 0; y = !x; dest[x] = *ptr; ptr += 1; dest[y] = *ptl; ptl += 1; dest += 2;
-				x = cmp(ptl, ptr) <= 0; y = !x; dest[x] = *ptr; ptr += y; dest[y] = *ptl; ptl += x; dest++;
-			}
-		}
-
-		while (ptl <= m)
-		{
-			*dest++ = cmp(ptl, ptr) <= 0 ? *ptl++ : *ptr++;
-		}
-
-		do *dest++ = *ptr++; while (ptr <= e);
+		return FUNC(parity_merge)(dest, from, block, block * 2, cmp);
 	}
-	else if (cmp(m - block / 4, e) > 0)
+
+	ptd = dest;
+	tpd = dest + 2 * block - 1;
+
+	while (tpl - ptl > 8 && tpr - ptr > 8)
 	{
-		while (ptr < e - 1)
+		if (cmp(ptl + 7, ptr) <= 0)
 		{
-			if (cmp(ptl, ptr + 1) > 0)
-			{
-				*dest++ = *ptr++; *dest++ = *ptr++;
-			}
-			else if (cmp(ptl + 1, ptr) <= 0)
-			{
-				*dest++ = *ptl++; *dest++ = *ptl++;
-			}
-			else 
-			{
-				x = cmp(ptl, ptr) <= 0; y = !x; dest[x] = *ptr; ptr += 1; dest[y] = *ptl; ptl += 1; dest += 2;
-				x = cmp(ptl, ptr) <= 0; y = !x; dest[x] = *ptr; ptr += y; dest[y] = *ptl; ptl += x; dest++;
-			}
+			loop = 8; do *ptd++ = *ptl++; while (--loop); continue;
 		}
-
-		while (ptr <= e)
+		if (cmp(ptl, ptr + 7) > 0)
 		{
-			*dest++ = cmp(ptl, ptr) > 0 ? *ptr++ : *ptl++;
+			loop = 8; do *ptd++ = *ptr++; while (--loop); continue;
 		}
-
-		do *dest++ = *ptl++; while (ptl <= m);
+		if (cmp(tpl, tpr - 7) <= 0)
+		{
+			loop = 8; do *tpd-- = *tpr--; while (--loop); continue;
+		}
+		if (cmp(tpl - 7, tpr) > 0)
+		{
+			loop = 8; do *tpd-- = *tpl--; while (--loop); continue;
+		}
+		loop = 8; do
+		{
+			x = cmp(ptl, ptr) <= 0; y = !x; ptd[x] = *ptr; ptr += y; ptd[y] = *ptl; ptl += x; ptd++;
+			x = cmp(tpl, tpr) <= 0; y = !x; tpd--; tpd[x] = *tpr; tpr -= x; tpd[y] = *tpl; tpl -= y;
+		}
+		while (--loop);
 	}
-	else
+
+	while (tpl - ptl > 1 && tpr - ptr > 1)
 	{
-		FUNC(parity_merge)(dest, from, block, block * 2, cmp);
+		if (cmp(ptl + 1, ptr) <= 0)
+		{
+			*ptd++ = *ptl++; *ptd++ = *ptl++;
+		}
+		else if (cmp(ptl, ptr + 1) > 0)
+		{
+			*ptd++ = *ptr++; *ptd++ = *ptr++;
+		}
+		else 
+		{
+			x = cmp(ptl, ptr) <= 0; y = !x; ptd[x] = *ptr; ptr += 1; ptd[y] = *ptl; ptl += 1; ptd += 2;
+			x = cmp(ptl, ptr) <= 0; y = !x; ptd[x] = *ptr; ptr += y; ptd[y] = *ptl; ptl += x; ptd++;
+		}
+	}
+
+	while (ptl <= tpl && ptr <= tpr)
+	{
+		*ptd++ = cmp(ptl, ptr) <= 0 ? *ptl++ : *ptr++;
+	}
+	while (ptl <= tpl)
+	{
+		*ptd++ = *ptl++;
+	}
+	while (ptr <= tpr)
+	{
+		*ptd++ = *ptr++;
 	}
 }
 
@@ -496,7 +503,7 @@ void FUNC(forward_merge)(VAR *dest, VAR *from, size_t block, CMPFUNC *cmp)
 
 void FUNC(quad_merge_block)(VAR *array, VAR *swap, size_t block, CMPFUNC *cmp)
 {
-	register VAR *pts, *c, *c_max;
+	VAR *pts, *c, *c_max;
 	size_t block_x_2 = block * 2;
 
 	c_max = array + block;
@@ -523,7 +530,7 @@ void FUNC(quad_merge_block)(VAR *array, VAR *swap, size_t block, CMPFUNC *cmp)
 
 			do *pts++ = *c++; while (c < c_max); // step 2
 
-			return FUNC(forward_merge)(array, swap, block_x_2, cmp); // step 3
+			return FUNC(galloping_merge)(array, swap, block_x_2, cmp); // step 3
 		}
 		pts = swap;
 
@@ -534,16 +541,16 @@ void FUNC(quad_merge_block)(VAR *array, VAR *swap, size_t block, CMPFUNC *cmp)
 	}
 	else
 	{
-		FUNC(forward_merge)(swap, array, block, cmp); // step 1
+		FUNC(galloping_merge)(swap, array, block, cmp); // step 1
 	}
-	FUNC(forward_merge)(swap + block_x_2, array + block_x_2, block, cmp); // step 2
+	FUNC(galloping_merge)(swap + block_x_2, array + block_x_2, block, cmp); // step 2
 
-	FUNC(forward_merge)(array, swap, block_x_2, cmp); // step 3
+	FUNC(galloping_merge)(array, swap, block_x_2, cmp); // step 3
 }
 
 size_t FUNC(quad_merge)(VAR *array, VAR *swap, size_t swap_size, size_t nmemb, size_t block, CMPFUNC *cmp)
 {
-	register VAR *pta, *pte;
+	VAR *pta, *pte;
 
 	pte = array + nmemb;
 
@@ -661,7 +668,7 @@ void FUNC(partial_backward_merge)(VAR *array, VAR *swap, size_t nmemb, size_t bl
 
 void FUNC(tail_merge)(VAR *array, VAR *swap, size_t swap_size, size_t nmemb, size_t block, CMPFUNC *cmp)
 {
-	register VAR *pta, *pte;
+	VAR *pta, *pte;
 
 	pte = array + nmemb;
 
